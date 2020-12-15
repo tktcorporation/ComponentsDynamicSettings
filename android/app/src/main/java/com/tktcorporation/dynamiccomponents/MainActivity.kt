79b8910b3120c13effe51744950dc63e593bad7b
tkt <tktcorporation.go@gmail.com>
2020-12-16 01:08:05 +0900

    success to change a button color intaractive

--- android/app/src/main/java/com/tktcorporation/dynamiccomponents/MainActivity.kt
+++ android/app/src/main/java/com/tktcorporation/dynamiccomponents/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.tktcorporation.dynamiccomponents
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tktcorporation.dynamiccomponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.changeButton.setOnClickListener {
+            val colorString = binding.colorCode.text.toString()
+            binding.customButton.setBackgroundColor(Color.parseColor(colorString))
+            binding.customButton.setTextColor(Color.parseColor(colorString))
+        }
     }
 }
